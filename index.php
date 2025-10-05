7f11736c71a76ab2944847aa42d3a0d3a2ccdfc7
howwbe <firmansyahfahmi802@gmail.com>
2025-10-05 11:54:06 +0700

    2

--- index.php
+++ index.php
@@ -121,6 +121,17 @@ require_once 'koneksi.php';
     <!-- AWAL: DAFTAR BERITA (GABUNGAN HTML DAN PHP) -->
     <!-- Sama seperti slider, PHP akan mengulang data berita dan mencetaknya dalam bentuk card. -->
     <!-- =================================================================== -->
+      <section class="link-terkait">
+    <h3>Link Terkait</h3>
+    <div class="images">
+      <a href="https://lapor.go.id/"><img src="logo/lapor1.png" alt="lapor"></a>
+      <a href="https://laporpakyes.lamongankab.go.id/"><img src="logo/lpryes.png" alt="lapor pak yes"></a>
+      <a href="https://laporwbs.lamongankab.go.id/"><img src="logo/wbs.png" alt="lapor wbs"></a>
+      <a href="https://lamongankab.go.id/artikel/47"><img src="logo/nomor.png" alt="nomor penting"></a>
+    </div>
+  </section>
+
+  <!-----berita----->
     <section id="berita-utama">
         <div class="container">
             <h2>Berita Terkini</h2>
